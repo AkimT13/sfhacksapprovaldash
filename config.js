8971e3d68a6617d89343dd790644cd738dd2c535
AkimT13 <akimtarasov@gmail.com>
2024-11-17 21:13:34 -0800

    firebase setup

---
+++ config.js
@@ -0,0 +1,58 @@
+import { initializeApp } from 'firebase-admin/app';
+import admin from 'firebase-admin'
+
+import { getDatabase, ref,set,remove } from "firebase/database";
+
+import { readFileSync } from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config()
+const key = process.env.ADMIN_INFO
+const serviceAccount = JSON.parse(Buffer.from(key,'base64').toString('utf-8'))
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://sfhacks2025dev-default-rtdb.firebaseio.com"
+});
+const database = admin.database();
+
+
+//test writing/reading from database
+const write = (s, id)=>{
+    set(ref(database,'blah/' + id), {
+        word: s
+    });
+    console.log("successfully written")
+    return
+
+
+}
+
+const clearDb = ()=>{ // if you run this function it will fucking delete everything
+  const responsesRef = ref(database, 'responses');
+  const teamsRef = ref(database, 'teams')
+  remove(responsesRef)
+  .then(() => {
+    console.log('Responses node deleted successfully.');
+  })
+  .catch((error) => {
+    console.error('Error deleting responses node:', error);
+  });
+  remove(teamsRef)
+  .then(() => {
+    console.log('Responses node deleted successfully.');
+  })
+  .catch((error) => {
+    console.error('Error deleting responses node:', error);
+  });
+  
+}
+
+write("hello", 123)
+
+
+
+
+
+
+export {database}
